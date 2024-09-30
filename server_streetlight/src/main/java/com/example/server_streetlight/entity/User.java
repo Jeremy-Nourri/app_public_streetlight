8cc077f081ebc53da32d9f77a4e896702991891f
Anthony Hayart <an.hayart@gmail.com>
2024-09-30 14:49:15 +0200

    working security

---
+++ server_streetlight/src/main/java/com/example/server_streetlight/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.server_streetlight.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
