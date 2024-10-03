b66d428cd5e424b5c88879b0c60d87447d9af06e
Administrateur <nourri.jer@gmail.com>
2024-10-03 11:10:39 +0200

    feat: add day and night image, and time

--- client/src/stores/streetlightStore.js
+++ client/src/stores/streetlightStore.js
@@ -3,9 +3,11 @@ import { defineStore } from 'pinia';
 export const useStreetlightStore = defineStore('streetlight', {
     state: () => ({
         isActive: false,
+        time: null
     }),
     getters: {
-        isActive: (state) => state.isActive
+        getIsActive: (state) => state.isActive,
+        getTime: (state) => state.time
     },
     actions: {
         async fetchStreetlightStatusById(id) {
@@ -14,14 +16,17 @@ export const useStreetlightStore = defineStore('streetlight', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json', 
-
                     }
                 });
         
                 if (response.ok) {
                     const data = await response.json();
-                    this.isActive = data;
-                    
+
+                    if (typeof data === 'boolean') {
+                        this.$patch({ isActive: data });
+                    } else {
+                        console.error('Données inattendues reçues:', data);
+                    }
                 } else {
                     console.error('Erreur lors de la récupération de l\'état du lampadaire.');
                 }
@@ -29,13 +34,40 @@ export const useStreetlightStore = defineStore('streetlight', {
                 console.error('Erreur de réseau:', error);
             }
         },
-        
 
-        startFetchingStatus(id) {
+        async fetchTime() {
+            try {
+                const response = await fetch('http://localhost:8080/api/streetlights/time', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json', 
+                    }
+                });
+        
+                if (response.ok) {
+                    const data = await response.json();
+
+                    if (typeof data === 'number') {
+                        this.$patch({ time: data });
+                    } else {
+                        console.error('Données inattendues reçues:', data);
+                    }
+
+                } else {
+                    console.error('Erreur lors de la récupération de l\'heure actuelle.');
+                }
+            } catch (error) {
+                console.error('Erreur de réseau:', error);
+            }
+        },
+
+        startFetchingStatusAndTime(id) {
             this.fetchStreetlightStatusById(id);
+            this.fetchTime();
             setInterval(() => {
                 this.fetchStreetlightStatusById(id);
-            }, 10000);
+                this.fetchTime();
+            }, 5000);
         }
     }
 });
